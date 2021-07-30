99e835094acdb3136ff392f6f562713d3c3e4e38
Warren Thompson (wtvamp) <warrenjthompson@gmail.com>
2021-07-30 11:52:31 -0700

    Refactoring printing to cart

--- Cart.cs
+++ Cart.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 
 public class Cart {
 
     List<StoreItem> storeItemsToPurchase;
+    private decimal total;
     
     public Cart()
     {
@@ -11,5 +13,19 @@ public class Cart {
 
     public void AddToCart(StoreItem itemToAdd) {
         storeItemsToPurchase.Add(itemToAdd);
+    }
+
+    public void PrintItems() {
+        Console.WriteLine("Thank you for shopping with us. Here are your items and total:");
+        Console.WriteLine("--------------------------------------------------------");
+        Console.WriteLine(String.Format("{0,-45} | {1,-10}", "Item" , "Price" ));
+        Console.WriteLine("--------------------------------------------------------");
+        foreach (var i in storeItemsToPurchase)
+        {
+            Console.WriteLine(String.Format("{0,-45} | {1,-10}", i.Name , "$" + i.Price ));
+            total = total + i.Price;
+        }
+        Console.WriteLine("--------------------------------------------------------");
+        Console.WriteLine("Total: $" + total);
     }
 }
